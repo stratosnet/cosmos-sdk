d9656d81fc544f1b949fea89c9a5c7477efb7d75
Xiong <xixiongchen@thestratos.org>
2024-04-26 15:12:32 -0400

    fix duplicated validators

--- x/staking/keeper/val_state_change.go
+++ x/staking/keeper/val_state_change.go
@@ -194,7 +194,6 @@ func (k Keeper) ApplyAndReturnValidatorSetUpdates(ctx sdk.Context) (updates []ab
 
 		// update the validator set if power has changed
 		if !found || !bytes.Equal(oldPowerBytes, newPowerBytes) {
-			updates = append(updates, validator.ABCIValidatorUpdate(powerReduction))
 
 			k.SetLastValidatorPower(ctx, valAddr, newPower)
 
